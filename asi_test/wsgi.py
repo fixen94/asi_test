2d14083d89a98f50e2e4b5689c00e5436fb0a5e2
Dmitry Bukholtsev <fixen94@gmail.com>
2019-08-23 15:44:35 +1000

    Update wsgi.py

--- asi_test/wsgi.py
+++ asi_test/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asi_test.settings')
 
 application = get_wsgi_application()
+
+application = DjangoWhiteNoise(application)
